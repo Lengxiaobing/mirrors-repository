21cebc861e51c3816e24f7c541f04a1fccd62a80
root <tn787681084@gmail.com>
2019-10-12 11:20:53 +0800

    下载从节点镜像

--- Kubernetes/install/install-k8s/k8s-images-node.sh
+++ Kubernetes/install/install-k8s/k8s-images-node.sh
@@ -5,16 +5,12 @@
 k8s_version=v1.15.3
 pause_version=3.1
 
-calico_version=v3.8.2
-flannel_version=v0.11.0
 dashboard_version=v2.0.0-beta4
 metrics_scraper_version=v1.0.1
 
 # 路径
 k8s_path=k8s.gcr.io
 dashboard_path=kubernetesui
-calico_path=calico
-flannel_path=quay.io/coreos
 docker_path=lengxiaobing
 
 # 拉取k8s镜像文件
@@ -25,17 +21,6 @@ function pull_images(){
 
     sudo docker pull ${docker_path}/dashboard:${dashboard_version}
     sudo docker pull ${docker_path}/metrics-scraper:${metrics_scraper_version}
-
-    sudo docker pull ${docker_path}/calico-cni:${calico_version}
-    sudo docker pull ${docker_path}/calico-node:${calico_version}
-    sudo docker pull ${docker_path}/calico-kube-controllers:${calico_version}
-    sudo docker pull ${docker_path}/calico-pod2daemon-flexvol:${calico_version}
-
-    sudo docker pull ${docker_path}/flannel-amd64:${flannel_version}-amd64
-    sudo docker pull ${docker_path}/flannel-arm64:${flannel_version}-arm64
-    sudo docker pull ${docker_path}/flannel-arm:${flannel_version}-arm
-    sudo docker pull ${docker_path}/flannel-ppc64le:${flannel_version}-ppc64le
-    sudo docker pull ${docker_path}/flannel-s390x:${flannel_version}-s390x
 }
 
 # 设置tag
@@ -47,34 +32,12 @@ function reset_tags(){
     sudo docker tag ${docker_path}/dashboard:${dashboard_version}               ${dashboard_path}/dashboard:${dashboard_version}
     sudo docker tag ${docker_path}/metrics-scraper:${metrics_scraper_version}   ${dashboard_path}/metrics-scraper:${metrics_scraper_version}
 
-    sudo docker tag ${docker_path}/calico-cni:${calico_version}					${calico_path}/cni:${calico_version}
-    sudo docker tag ${docker_path}/calico-node:${calico_version}				${calico_path}/node:${calico_version}
-    sudo docker tag ${docker_path}/calico-kube-controllers:${calico_version}	${calico_path}/kube-controllers:${calico_version}
-    sudo docker tag ${docker_path}/calico-pod2daemon-flexvol:${calico_version}	${calico_path}/pod2daemon-flexvol:${calico_version}
-
-    sudo docker tag ${docker_path}/flannel-amd64:${flannel_version}-amd64		${flannel_path}/flannel:${flannel_version}-amd64
-    sudo docker tag ${docker_path}/flannel-arm64:${flannel_version}-arm64		${flannel_path}/flannel:${flannel_version}-arm64
-    sudo docker tag ${docker_path}/flannel-arm:${flannel_version}-arm			${flannel_path}/flannel:${flannel_version}-arm
-    sudo docker tag ${docker_path}/flannel-ppc64le:${flannel_version}-ppc64le	${flannel_path}/flannel:${flannel_version}-ppc64le
-    sudo docker tag ${docker_path}/flannel-s390x:${flannel_version}-s390x		${flannel_path}/flannel:${flannel_version}-s390x
-
     echo "Delete Tags"
     sudo docker rmi  ${docker_path}/kube-proxy:${k8s_version}
     sudo docker rmi  ${docker_path}/pause:${pause_version}
 
     sudo docker rmi  ${docker_path}/dashboard:${dashboard_version}
     sudo docker rmi  ${docker_path}/metrics-scraper:${metrics_scraper_version}
-
-    sudo docker rmi  ${docker_path}/calico-cni:${calico_version}
-    sudo docker rmi  ${docker_path}/calico-node:${calico_version}
-    sudo docker rmi  ${docker_path}/calico-kube-controllers:${calico_version}
-    sudo docker rmi  ${docker_path}/calico-pod2daemon-flexvol:${calico_version}
-
-    sudo docker rmi  ${docker_path}/flannel-amd64:${flannel_version}-amd64
-    sudo docker rmi  ${docker_path}/flannel-arm64:${flannel_version}-arm64
-    sudo docker rmi  ${docker_path}/flannel-arm:${flannel_version}-arm
-    sudo docker rmi  ${docker_path}/flannel-ppc64le:${flannel_version}-ppc64le
-    sudo docker rmi  ${docker_path}/flannel-s390x:${flannel_version}-s390x
 }
 
 #server
